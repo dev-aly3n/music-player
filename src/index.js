d39d5934bce7e4d7b43ea88c147de6d56745e38e
aly3n <aly3n.dev@gmail.com>
2021-05-12 13:54:06 +0430

    basic syles of the player and cover added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
 import './styles/tailwind.output.css';
+import App from './App';
 
-const App = () => (
-  <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-    <div className="ml-6 pt-1">
-      <h1 className="text-2xl text-blue-700 leading-tight bg-gre">
-        Tailwind and Create React App
-      </h1>
-      <p className="text-base text-gray-700 leading-normal">
-        Building apps together
-      </p>
-    </div>
-  </div>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
