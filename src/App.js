bd59c656a0e20686dfd261a4bebcc6585973f22b
aly3n <aly3n.dev@gmail.com>
2021-05-13 09:08:47 +0430

    music data file added to the project and song component render some data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
+import data from './MusicData';
 
 function App() {
+
+  const [songs, SETsongs] = useState(data());
+  const [currentSong, SETcurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player/>
       
      </div>
