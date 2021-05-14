2f2ec9c5ad6b624617fca6746650e2386981c10d
aly3n <aly3n.dev@gmail.com>
2021-05-14 09:34:07 +0430

    library interface added to the project

--- src/components/Library.js
+++ src/components/Library.js
@@ -6,7 +6,7 @@ function Library ({songs}){
 
     return (
         <div className="library">
-            <h2>Library</h2>
+            <h2 className=" text-2xl font-bold pl-6 py-3" >Library</h2>
             <div className="library-songs">
                 {songs.map(song => <LibrarySong song={song} />)}
 
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-const LibrarySong = ({song}) => {
+const LibrarySong = ({ song }) => {
   return (
-    <div className="library-song">
+    <div className="library-song-description">
       <img alt={song.name} src={song.cover}></img>
-      <h2>{song.name}</h2>
-      <h3>{song.artist}</h3>
+      <div className="song-info">
+        <h3 className=" text-lg  font-semibold text-gray-600 ml-2 mt-1">{song.name}</h3>
+        <h4 className=" text-sm font-normal text-gray-600 ml-2 mt-1">{song.artist}</h4>
+      </div>
     </div>
   );
 };
